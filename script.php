46c2f080e6bb4fd9427cab47c5a6d211f2dbda33
Vinicius Callegari <callegari@hotmail.com>
2021-06-09 22:36:45 -0300

    adicionando validação de campos no formulário.

--- script.php
+++ script.php
@@ -7,6 +7,28 @@ $categorias[] = 'Infantil';
 $categorias[] = 'Adolecente';
 $categorias[] = 'Adulto';
 
+if(empty($nome)){
+    echo 'O nome não pode ser vazio.';
+    return;
+}
+if(strlen($nome) < 3){
+    echo 'O nome deve conter mais de 3 caracteres.';
+    return;
+}
+if(strlen($nome) > 40){
+    echo 'O nome é muito extenso';
+    return;
+}
+
+if(!is_numeric($idade)){
+    echo 'Informe um numero para idade.';
+    return;
+}
+if(strlen($idade) > 3){
+    echo 'A idade de conter no maximo 3 digitos númericos.';
+    return;
+}
+
 // var_dump($nome);
 // var_dump($idade);
 
